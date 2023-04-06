3bf14baf59198de3b45850ebb02777e8ed8a4f92
Reed W Priest <pries153@umn.edu>
2023-04-06 13:06:50 -0500

    tic() and toc() applied to local models.

--- R/11-local.R
+++ R/11-local.R
@@ -3,17 +3,20 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(haven)
 library(caret)
+library(tictoc)
 set.seed(123)
 
 ## Data Import and Cleaning
 gss_tbl <- read_spss("../data/GSS2016.sav") %>%
-  filter(!is.na(HRS1)) %>%
+  # Remove the other two columns that predict a similar criterion.
+  select(-HRS1, -HRS2) %>%
+  filter(!is.na(MOSTHRS)) %>%
   sapply(as.numeric) %>%
   as_tibble() %>%
   select_if(~(mean(is.na(.)) * 100) <= 75)
 
 ## Visualization
-(ggplot(gss_tbl, aes(x = HRS1)) +
+(ggplot(gss_tbl, aes(x = MOSTHRS)) +
     geom_histogram(binwidth = 10) +
     labs(x = "Workhours", y = "Count")) %>%
   ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
@@ -22,45 +25,54 @@ gss_tbl <- read_spss("../data/GSS2016.sav") %>%
 gss_random_tbl <- gss_tbl[sample(nrow(gss_tbl)), ]
 gss_random75 <- round(nrow(gss_random_tbl) * 0.75, 0)
 gss_train_tbl <- gss_random_tbl[1:gss_random75, ]
-kfolds <- createFolds(gss_train_tbl$HRS1, 10)
+kfolds <- createFolds(gss_train_tbl$MOSTHRS, 10)
 gss_test_tbl <- gss_random_tbl[(gss_random75 + 1):nrow(gss_random_tbl), ]
 
+# The tic() toc() functions tell us how much time passed to execute all code around which they are wrapped. They are used to see how efficient our run time is. Importantly, we save the amount of time they calculate to an object to be used later in our table2_tbl. tic() and toc() are used frequently throughout this code, but only explained in a comment here for the sake of simplicity. 
+tic()
 OLS <- train(
-  HRS1 ~ .,
+  MOSTHRS ~ .,
   gss_train_tbl,
   method = "lm",
   na.action = na.pass,
   preProcess = c("center", "scale", "nzv", "medianImpute"),
   trControl = trainControl(method = "cv", indexOut = kfolds, number = 10, search = "grid", verboseIter = TRUE)
 )
+tocOLS <- toc()
 OLS
 
+tic()
 Enet <- train(
-  HRS1 ~ .,
+  MOSTHRS ~ .,
   gss_train_tbl,
   method = "glmnet",
   na.action = na.pass,
   preProcess = c("center", "scale", "nzv", "medianImpute"),
   trControl = trainControl(method = "cv", indexOut = kfolds, number = 10, search = "grid", verboseIter = TRUE)
 )
+tocEnet <- toc()
 Enet
 
+tic()
 random_forest_gump <- train(
-  HRS1 ~ .,
+  MOSTHRS ~ .,
   gss_train_tbl,
   method = "ranger",
   na.action = na.pass,
   preProcess = c("center", "scale", "nzv", "medianImpute"),
   trControl = trainControl(method = "cv", indexOut = kfolds, number = 10, search = "grid", verboseIter = TRUE))
+random_forest_gumptoc <- toc()
 random_forest_gump
 
+tic()
 EGB <- train(
-  HRS1 ~ .,
+  MOSTHRS ~ .,
   gss_train_tbl,
   method = "xgbLinear", 
   na.action = na.pass,
   preProcess = c("center", "scale", "nzv", "medianImpute"),
   trControl = trainControl(method = "cv", indexOut = kfolds, number = 10, search = "grid", verboseIter = TRUE))
+EGBtoc <- toc()
 EGB
 
 FirstR2 <- OLS$results$Rsquared %>%
@@ -76,23 +88,27 @@ FourthR2 <- max(EGB$results$Rsquared) %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
 
-holdout1 <- cor(predict(OLS, gss_test_tbl, na.action = na.pass), gss_test_tbl$HRS1)^2  %>%
+holdout1 <- cor(predict(OLS, gss_test_tbl, na.action = na.pass), gss_test_tbl$MOSTHRS)^2  %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0") 
-holdout2 <- cor(predict(Enet, gss_test_tbl, na.action = na.pass), gss_test_tbl$HRS1)^2  %>%
+holdout2 <- cor(predict(Enet, gss_test_tbl, na.action = na.pass), gss_test_tbl$MOSTHRS)^2  %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
-holdout3 <- cor(predict(random_forest_gump, gss_test_tbl, na.action = na.pass), gss_test_tbl$HRS1)^2  %>%
+holdout3 <- cor(predict(random_forest_gump, gss_test_tbl, na.action = na.pass), gss_test_tbl$MOSTHRS)^2  %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
-holdout4 <- cor(predict(EGB, gss_test_tbl, na.action = na.pass), gss_test_tbl$HRS1)^2 %>%
+holdout4 <- cor(predict(EGB, gss_test_tbl, na.action = na.pass), gss_test_tbl$MOSTHRS)^2 %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
 
 ## Publication
-
 table1_tbl <- tibble(
   algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"),
   cv_rsq = c(FirstR2, SecondR2, ThirdR2, FourthR2),
   ho_rsq = c(holdout1, holdout2, holdout3, holdout4)
 )
+
+# This series of pipes creates a new tibble that outlines the run times of our various models when executed either normally or with parallelization. We do this to compare efficiency increases in run time.
+table2_tbl <- tibble(algo = c("OLS Regression", "Elastic Net", "Random Forest", "eXtreme Gradient Boosting"),
+                original = ,
+                parallelized = )
